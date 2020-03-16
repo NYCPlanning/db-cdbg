c5fed00439478ffc8347314440d3ef7293f98fca
mgraber <molly.graber@me.com>
2020-03-16 19:26:52 +0000

    Rename columns and add tract info

--- python/format.py
+++ python/format.py
@@ -31,31 +31,31 @@ df = df.merge(project_code[['program_name', 'project_code', 'join_key']],
 
 df.fillna('', inplace=True)
 df['boroct'] = list(map(lambda boro, ct: boro+ct if ct!='' else '', df.boro, df.geo_censtract))
-col_names = {'project_id': 'PROJCODE',
-             'units':'UNIT SUM',
-             'input_type_code':'CODE',
-             'boro':'BORO',
-             'address': 'ADDRESS',
-             'geo_address':'ADDRESS GBAT',
-             'geo_censtract':'CT GBAT', 
-             'geo_borough_code':'BORO GBAT',
-             'boroct': 'BORO CT',
-             'geo_commboard':'COMM DIST GBAT', 
-             'geo_council': 'COUNCIL DIST GBAT',
-             'geo_cong':'CONG DIST GBAT',
-             'tax_block':'BLOCK', 
-             'tax_lot':'LOT',
-             'geo_block':'BLOCK GBAT',
-             'geo_lot':'LOT GBAT', 
-             'geo_stcode':'STCODE GBAT', 
-             'geo_bin':'BIN GBAT',
-             'program_name':'PROJNAME',
-             'geo_street_name':'STREET NAME GBAT',
-             'geo_house_number':'HSNUM GBAT',
-             'hnum': 'HSNUM',
-             'sname': 'STNAME',
+col_names = {'project_id': 'Project_name',
+             'units':'Units',
+             'input_type_code':'Type',
+             'boro':'Borocode',
+             'address': 'Address',
+             'geo_address':'GBAT_Address',
+             'geo_censtract':'GBAT_tract', 
+             'geo_borough_code':'GBAT_boro',
+             'boroct': 'GBAT_borotract',
+             'geo_commboard':'GBAT_commdist', 
+             'geo_council': 'GBAT_councildist',
+             'geo_cong':'GBAT_congdist',
+             'tax_block':'Block', 
+             'tax_lot':'Lot',
+             'geo_block':'GBAT_block',
+             'geo_lot':'GBAT_lot', 
+             'geo_stcode':'GBAT_streetnum', 
+             'geo_bin':'GBAT_BIN',
+             'program_name':'Project_name',
+             'geo_street_name':'GBAT_street',
+             'geo_house_number':'GBAT_num',
+             'hnum': 'num',
+             'sname': 'street',
              'bbl':'BBL',
-             'geo_bbl':'BBL GBAT'}
+             'geo_bbl':'GBAT_BBL'}
 
 df = df.rename(columns=col_names)
 df = df[list(col_names.values())]
@@ -75,12 +75,19 @@ def get_boroct(bbl):
         result = r[0][0]
         return result
 
-df.loc[(df['BORO CT'].isna())&(~df.BBL.isna()), 'BORO CT'] = df.loc[(df['BORO CT'].isna())&(~df.BBL.isna()), 'BBL'].apply(get_boroct)
+df.loc[(df['GBAT_borotract'].isna())&(~df.BBL.isna()), 'GBAT_borotract'] = df.loc[(df['GBAT_borotract'].isna())&(~df.BBL.isna()), 'BBL'].apply(get_boroct)
 df.to_csv(f'{Path(__file__).parent.parent}/output/CDBG_2020.csv', index=False)
 
-## Adding eligibility field
+## Adding census tract fields
 df = pd.read_csv(f'{Path(__file__).parent.parent}/output/CDBG_2020.csv', index_col=False, dtype=str)
 eligibility = pd.read_csv(f'{Path(__file__).parent.parent}/data/CDBG_census_tract.csv', index_col=False, dtype=str)
-df = df.merge(eligibility[['BoroCT', 'Eligibility']], how='left', left_on='BORO CT', right_on='BoroCT')
-df = df.drop(columns=['BoroCT'],  axis=1)
+df = df.merge(eligibility[['BoroCT', 'Eligibility', 'TotalPop', 'LowMod_Population', 'Res_pct']], how='left', left_on='GBAT_borotract', right_on='BoroCT')
+df = df.drop(columns=['BoroCT'],  axis=1).rename(columns = {'Eligibility':'CD_Eligibility', 
+                                                            'TotalPop':'Total_Persons',
+                                                            'LowMod_Population':'LowMod_Persons',
+                                                            'Res_pct':'Per_Residential'})
+
+
+
+
 df.to_csv(f'{Path(__file__).parent.parent}/output/CDBG_2020_FINAL.csv', index=False)
